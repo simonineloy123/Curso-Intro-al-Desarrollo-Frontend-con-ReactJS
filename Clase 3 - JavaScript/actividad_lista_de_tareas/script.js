a4a6a89faf3cd8514851058a5093bee6c46ca01c
simonineloy123 <simonineloy123@gmail.com>
2023-09-17 16:43:25 -0300

    Actividad 3 - To Do List con JavaScript

---
+++ Clase 3 - JavaScript/actividad_lista_de_tareas/script.js
@@ -0,0 +1,56 @@
+
+//Funcion para agregar una nueva tarea
+function agregarTarea() {
+
+
+    const  nuevaTareaInput = document.getElementById('nuevaTarea');
+    const listaTareas = document.getElementById('listaTareas');
+    const nuevaTareaTexto = nuevaTareaInput.value.trim();
+
+    //Cambiamos la logica del IF para que no permita introducir tareas con nombre vacio
+    if(nuevaTareaInput.value.length > 0) {
+
+        //Creamos una tarea de tipo li que se agrega a la lista
+        const nuevaTarea = document.createElement('li');
+        nuevaTarea.textContent = nuevaTareaTexto;
+
+        const botonEliminar = document.createElement("button");
+        botonEliminar.textContent = "Eliminar";
+        botonEliminar.className = 'delete';
+        botonEliminar.onclick = function() {
+            listaTareas.removeChild(nuevaTarea);
+        };
+
+        nuevaTarea.appendChild(botonEliminar);
+        listaTareas.appendChild(nuevaTarea);
+
+        nuevaTareaInput.value = "";
+    }
+
+}
+
+//Funcion para marcar una tarea como completada
+function marcarCompletada(tarea) {
+    tarea.classList.toggle("completed");
+}
+
+//Funcion para agregar evento click a las tareas cuando se marcan como completadas
+document.getElementById("listaTareas").addEventListener("click", function(event) {
+    
+    if(event.target.tagName == "LI") {
+        marcarCompletada(event.target);
+    }
+});
+
+//Agregar evento click al boton "Agregar"
+document.getElementById("agregar").addEventListener("click", agregarTarea());
+
+
+// Función para eliminar el elemento li, que aparece al cargar la página
+document.addEventListener('DOMContentLoaded', function() {
+    var elementoAutomatico = document.querySelector('ul > li:first-child');
+     if (elementoAutomatico) {
+         elementoAutomatico.parentNode.removeChild(elementoAutomatico);
+    }
+});
+
